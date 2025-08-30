3e66a16676ea89a1ce22281b911624776b14e274
Ray Kwan <rkwan@skatescribe.com>
2025-08-29 20:45:06 -0400

    only show mini player in certain views

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,12 +5,12 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from '@react-navigation/native';
+import { Stack, useSegments } from 'expo-router';
 
 import { MiniPlayer } from '@/components/MiniPlayer';
 import { PlaybackProvider } from '@/context/playbackContext';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
@@ -19,19 +19,23 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  const segments = useSegments(); // current route segments
+
+  if (!loaded) return null;
+
+  // Define pages where MiniPlayer should appear
+  const showMiniPlayer =
+    segments[0] === 'folder' ||
+    (segments[0] === '(tabs)' && segments[1] === 'folder_list');
 
   return (
     <PlaybackProvider>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <MiniPlayer />
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
+        {showMiniPlayer && <MiniPlayer />}
         <StatusBar style="auto" />
       </ThemeProvider>
     </PlaybackProvider>
